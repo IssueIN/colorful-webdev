f53525494cc6274c2df4d69e67c165d48d01cdda
Yixiu Yin <yixiuyin10@gmail.com>
2023-09-02 12:09:47 +0100

    auto generating carousel and product slides

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,33 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../models/product')
 const Manufacturer = require('../models/manufacturer')
+const fs = require('fs');
+const path = require('path');
 
 router.get('/', async (req, res) => {
   let productsRecent
   try {
     productsRecent = await Product.find().sort({publishedAt: 'desc'}).limit(10).exec()
-    res.render('index',{
-      searchOptions: req.query,
-      productsRecent: productsRecent,
+
+    const carouselPath = path.join(__dirname, '../public/uploads/carousel-img');
+
+    fs.readdir(carouselPath, (err, files) => {
+      if (err) {
+        console.error('Could not list the directory.', err);
+        return;
+      }
+      
+      const imageFiles = files.filter(file => ['.jpg', '.png', '.gif', '.jpeg'].includes(path.extname(file).toLowerCase()));
+      const imageCount = imageFiles.length;
+      const imagePaths = imageFiles.map(fileName => path.join(carouselPath, fileName));
+      const carouselImgPaths = pathModify(imagePaths);
+
+      res.render('index',{
+        searchOptions: req.query,
+        productsRecent: productsRecent,
+        carouselImgCount: imageCount,
+        carouselImgPaths: carouselImgPaths,
+      })
     })
   } catch {
     res.send('error entering main page!')
@@ -31,5 +50,12 @@ router.get('/join-us', (req, res) => {
   res.send('this is join us page')
 })
 
+function pathModify(paths) {
+  const baseDirectory = 'C:\\Users\\13410\\Desktop\\Website-coding\\colorful-website\\public';
+  const modifiedPaths = paths.map(path => 
+    path.replace(baseDirectory, '').replace(/\\/g, '/')
+  );
+  return modifiedPaths;
+}
 
 module.exports = router
